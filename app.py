dfa52bc92bfab0c1d3dfc63eb62e4a4c385dad2c
zengjn-2024 <zengjn@126.com>
2024-07-19 20:11:43 +0800

    Create,edit and delete item by form

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import os
 import sys
-from flask import Flask,render_template
-from flask_sqlalchemy import SQLAlchemy # 导入扩展类
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy  # 导入扩展类
 import click
-from flask import request,url_for,redirect,flash
+from flask import request, url_for, redirect, flash
 
-WIN=sys.platform.startswith('win')
+WIN = sys.platform.startswith('win')
 if WIN:  # 如果是 Windows 系统，使用三个斜线
     prefix = 'sqlite:///'
 else:  # 否则使用四个斜线
@@ -14,8 +14,9 @@ else:  # 否则使用四个斜线
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+app.config['SECRET_KEY'] = 'your_secret_key'
 # 在扩展类实例化前加载配置
-db=SQLAlchemy(app) # 初始化扩展，传入程序实例 app
+db = SQLAlchemy(app)  # 初始化扩展，传入程序实例 app
 
 
 class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
@@ -27,7 +28,7 @@ class Movie(db.Model):  # 表名将会是 movie
     id = db.Column(db.Integer, primary_key=True)  # 主键
     title = db.Column(db.String(60))  # 电影标题
     year = db.Column(db.String(4))  # 电影年份
-    
+
 
 @app.cli.command()  # 注册为命令，可以传入 name 参数来自定义命令
 @click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
@@ -69,15 +70,18 @@ def forge():
     db.session.commit()
     click.echo('Done.')
 
+
 @app.context_processor
 def inject_user():  # 函数名可以随意修改
     user = User.query.first()
     return dict(user=user)  # 需要返回字典，等同于 return {'user': user}
 
+
 @app.errorhandler(404)  # 传入要处理的错误代码
-def page_not_found(e):  # 接受异常对象作为参数
+def page_not_found():  # 接受异常对象作为参数
     user = User.query.first()
     return render_template('404.html'), 404  # 返回模板和状态码
+
 
 @app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
 def edit(movie_id):
@@ -99,6 +103,7 @@ def edit(movie_id):
 
     return render_template('edit.html', movie=movie)  # 传入被编辑的电影记录
 
+
 @app.route('/movie/delete/<int:movie_id>', methods=['POST'])  # 限定只接受 POST 请求
 def delete(movie_id):
     movie = Movie.query.get_or_404(movie_id)  # 获取电影记录
@@ -107,7 +112,8 @@ def delete(movie_id):
     flash('Item deleted.')
     return redirect(url_for('index'))  # 重定向回主页
 
-@app.route('/',methods=['GET','POST'])
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':  # 判断是否是 POST 请求
         # 获取表单数据
@@ -123,9 +129,10 @@ def index():
         db.session.commit()  # 提交数据库会话
         flash('Item created.')  # 显示成功创建的提示
         return redirect(url_for('index'))  # 重定向回主页
-        
-    movies=Movie.query.all()  # 读取所有电影记录
+
+    movies = Movie.query.all()  # 读取所有电影记录
     return render_template('index.html', movies=movies)
+
 
 if __name__ == "__main__":
     app.run()
